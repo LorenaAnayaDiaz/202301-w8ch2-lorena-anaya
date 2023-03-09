96aef3c2bbfc023b3fa077ee62496d524e4b8a67
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-09 13:14:08 +0100

    change user path to users

--- src/api/api-router.ts
+++ src/api/api-router.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import userRouter from '../api/users/users-router.js';
 
 const router = express.Router();
 
 // Router users
-
+router.use('/users', userRouter);
 export default router;
