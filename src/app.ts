5326ba1d41702461d2c8954bb978f04fd6368646
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-09 13:14:38 +0100

    add api router and api v1 path

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 
+import apiRouter from './api/api-router.js';
+
 const app = express();
 
 app.use(cors());
@@ -9,6 +11,9 @@ app.get('/', (req, res) => {
   res.json({ hello: 'World' });
 });
 app.use(express.json());
+
+app.use('/api/v1', apiRouter);
+
 app.disable('x-powered-by');
 
 export default app;
